b760c148b70619025ef1ce1390d382f0f1df2257
adwad3e-pixel <adwad3e@gmail.com>
2026-03-04 16:19:17 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+app.use(express.static(__dirname));
+
+let users = {};
+let buzzerLocked = false; 
+
+io.on('connection', (socket) => {
+    // تسجيل المستخدم
+    socket.on('registerUser', (data) => {
+        users[socket.id] = data;
+        io.emit('updateUserList', users);
+    });
+
+    // بدء اللعبة
+    socket.on('startGridGame', () => {
+        io.emit('showBuzzerScreen');
+    });
+
+    // عند ضغط البوزر
+    socket.on('pressBuzzer', () => {
+        if (!buzzerLocked) {
+            buzzerLocked = true; 
+            io.emit('buzzerWinner', { id: socket.id });
+
+            // إعادة ضبط تلقائي بعد 10 ثوانٍ
+            setTimeout(() => {
+                buzzerLocked = false;
+                io.emit('buzzerAutoReset'); 
+            }, 10000); 
+        }
+    });
+
+    // إعادة الضبط الكلي من المسؤول
+    socket.on('resetGame', () => {
+        users = {};
+        buzzerLocked = false;
+        io.emit('gameReset');
+    });
+
+    socket.on('disconnect', () => {
+        delete users[socket.id];
+        io.emit('updateUserList', users);
+    });
+});
+
+const PORT = 3000;
+http.listen(PORT, () => {
+    console.log(`✅ السيرفر يعمل على: http://localhost:${PORT}`);
+});
